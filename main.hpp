4163517737dcecf306fb8985a2a1c3cd3fd79437
Andrew Gaspar <agaspar2@students.avc.edu>
2025-04-25 20:22:40 -0700

    Quiz 6 Question 1

--- main.hpp
+++ main.hpp
@@ -1,8 +1,45 @@
 
-//********************
-// Declare Function Prototypes
-//********************
+#include <iostream>
+using namespace std;
 
-// ******************************
-// Implement all your functions here
-// ******************************
+
+void getTwoValues(int &begin, int &end) {
+    do {
+        cout << "Enter two integers (begin < end): ";
+        cin >> begin >> end;
+        if (begin >= end) {
+        cout << "Invalid input.\n";
+        }
+    } while (begin >= end); 
+}
+
+
+int isPrime(int num) {
+    if (num < 2) {
+        return 0; 
+    }
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return 0; 
+        }
+    }
+    return 1; 
+}
+
+
+int getNextPrime(int begin) {
+    int next = begin + 1; 
+    while (!isPrime(next)) {
+        ++next;
+    }
+    return next; 
+}
+
+
+int getPrevPrime(int end) {
+    int prev = end - 1; 
+    while (prev > 1 && !isPrime(prev)) {
+        --prev;
+    }
+    return prev; 
+}
